cae571b6494f7cf6cc0343b9b62e23da37cf3d57
Gustavo Di Martino <gustavodimartino@Gustavos-MBP.powerhub>
2025-08-26 14:31:50 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+            string player;
+            string computer;
+
+            while (playAgain)
+            {
+                player = "";
+                computer = "";
+                
+                while (player != "ROCK" && player != "PAPER" && player != "SCISSORS")
+                {
+                    Console.Write("Enter ROCK, PAPER or SCISSORS: ");
+                    player = Console.ReadLine().ToUpper();
+                }
+
+                Console.WriteLine(player);
+            }
+        }
+    }
+}
